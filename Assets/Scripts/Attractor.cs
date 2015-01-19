f14cf090f257346054a41babdf9fc9ba033eb0eb
wmatson <wmatson@users.noreply.github.com>
2015-01-19 07:32:46 -0700

    Added some rudimentary enemy movement

---
+++ Assets/Scripts/Attractor.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class Attractor : MonoBehaviour {
+
+	public GameObject target;
+	public float strengthMultiplier = 1;
+	public float distanceFactor = .5f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	void FixedUpdate () {
+		if (target == null)
+			return;
+		var direction = target.rigidbody2D.position -gameObject.rigidbody2D.position;
+		var strength = strengthMultiplier / (direction.sqrMagnitude * distanceFactor);
+		gameObject.rigidbody2D.AddForce (direction.normalized * strength);
+	}
+}
